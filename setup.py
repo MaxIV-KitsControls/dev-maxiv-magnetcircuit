d7983fcf2a19c0f2af2be588b45fd27edac26746
antdup <antoine.dupre@maxlab.lu.se>
2015-07-28 09:11:33 +0200

    bump version 1.6.2; use setutools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 setup(name = 'tangods-magnetcircuit',
-      version = '1.6.1',
+      version = '1.6.2',
       description = 'Tango device for magnet circuits and magnets',
       package_dir = {'MagnetCircuit':'src', 'MagnetCircuit.MagnetCycling':'src/cycling_statemachine'},
       packages = ['MagnetCircuit', 'MagnetCircuit.cycling_statemachine'],
