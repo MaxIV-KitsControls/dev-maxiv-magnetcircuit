e68f5d727ae64ee18ab03d6a12efa4fd6f39c7cf
antdup <antoine.dupre@maxlab.lu.se>
2015-08-06 09:02:39 +0200

    bump version 1.6.5

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup
 setup(name = 'tangods-magnetcircuit',
-      version = '1.6.4',
+      version = '1.6.5',
       description = 'Tango device for magnet circuits and magnets',
       package_dir = {'MagnetCircuit':'src', 'MagnetCircuit.MagnetCycling':'src/cycling_statemachine'},
       packages = ['MagnetCircuit', 'MagnetCircuit.cycling_statemachine'],
       author='Paul Bell',
       scripts = ['scripts/MagnetCircuit','scripts/Magnet'],
       )
+
